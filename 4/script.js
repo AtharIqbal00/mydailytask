d1d5fd0d629440b46b283e3e6c37b5bfae0fd109
AtharIqbal00 <atharhunzai1122@gmail.com>
2024-03-07 15:32:37 +0500

    CALLBACK HELL

--- 4/script.js
+++ 4/script.js
@@ -7,9 +7,13 @@ function getdata(dataId, getNextData) {
     }, 2000);
 }
 
+//CALLBACK HELL
 getdata(1, () => {
+    console.log("geting data 2");
     getdata(2, () => {
+        console.log("geting data 3");
         getdata(3, () => {
+            console.log("geting data 4");
             getdata(4);
         });
     });
